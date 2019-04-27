3c73c92bc751d564baef1fde7de0451c342b1d37
alicegherbison <alicegherbison@gmail.com>
2019-04-27 17:15:47 +0100

    Finish video 49

--- 14-es6-tooling/babel-test/app-compiled.js
+++ 14-es6-tooling/babel-test/app-compiled.js
@@ -1,5 +1,1 @@
-var age = 100;
-var people = ['Alice', 'Bob'];
-var names = people.map(function (name) {
-  return "".concat(name);
-});
+undefined
--- 14-es6-tooling/babel-test/app.js
+++ 14-es6-tooling/babel-test/app.js
@@ -2,3 +2,5 @@ const age = 100;
 const people = ['Alice', 'Bob'];
 
 const names = people.map(name => `${name}`);
+
+let { x, y, ...z } = { x: 1, y: 2, a: 3, b: 4 };
